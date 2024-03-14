32d766fbec6531cded11469f1dd3ad10dcdfba09
erickzz <erick.reis2@gmail.com>
2024-03-14 10:38:23 -0300

    Lidando com backgrounds claros e escuros

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -40,7 +40,22 @@ function Card({
   const cardId = cardOptions.id;
   const itensNumber = cardItems.length;
   const height = 280 + 70 * itensNumber;
+  const color = cardOptions.color;
   const [showMoveItem] = useState<number[]>([]);
+
+  const checkColorDark = (hexColor: string) => {
+    hexColor = hexColor.replace('#', '');
+
+    const r = parseInt(hexColor.substring(0, 2), 16);
+    const g = parseInt(hexColor.substring(2, 4), 16);
+    const b = parseInt(hexColor.substring(4, 6), 16);
+
+    const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+    return luminance < 128;
+  };
+
+  const isColorDark = checkColorDark(color);
 
   useEffect(() => {
     setCardItems(
@@ -78,7 +93,9 @@ function Card({
           <input
             id={item.id.toString()}
             defaultValue={item.value}
-            className="bg-transparent w-3/4 cursor-default p-2 focus:outline-double h-auto flex-wrap"
+            className={`${
+              isColorDark ? 'text-white' : 'text-zinc'
+            } bg-transparent w-3/4 cursor-default p-2 focus:outline-double h-auto flex-wrap`}
             onBlur={() => {
               if (editItemValue === '') {
                 (document.getElementById(
@@ -104,7 +121,11 @@ function Card({
             >
               <ArrowUp
                 size={20}
-                className="text-zinc-800 cursor-pointer hover:text-slate-200 transition-colors"
+                className={`${
+                  isColorDark
+                    ? 'text-white hover:text-zinc-800'
+                    : 'text-zinc hover:text-slate-200'
+                }   cursor-pointer  transition-colors`}
               />
             </button>
             <button
@@ -116,7 +137,11 @@ function Card({
             >
               <Pencil
                 size={20}
-                className="text-zinc-800 cursor-pointer hover:text-slate-200 transition-colors"
+                className={`${
+                  isColorDark
+                    ? 'text-white hover:text-zinc-800'
+                    : 'text-zinc hover:text-slate-200'
+                }   cursor-pointer  transition-colors`}
               />
             </button>
             <button
@@ -128,7 +153,11 @@ function Card({
             >
               <Trash
                 size={20}
-                className="text-zinc-800 cursor-pointer hover:text-slate-200 transition-colors"
+                className={`${
+                  isColorDark
+                    ? 'text-white hover:text-zinc-800'
+                    : 'text-zinc hover:text-slate-200'
+                }   cursor-pointer  transition-colors`}
               />
             </button>
           </div>
@@ -157,18 +186,29 @@ function Card({
     >
       <div
         className="flex justify-between w-full flex-wrap p-4 border-2 border-white rounded bg-white shadow-lg"
-        style={{ backgroundColor: cardOptions.color }}
+        style={{
+          backgroundColor: cardOptions.color,
+          color: isColorDark ? 'white' : 'black',
+        }}
       >
         <h3 className="text-xl font-semibold">{cardOptions.title}</h3>
         <div className="flex gap-4">
           <Pencil
-            className="text-zinc-800 cursor-pointer hover:text-slate-200 transition-colors"
+            className={`${
+              isColorDark
+                ? 'text-white hover:text-zinc-800'
+                : 'text-zinc hover:text-slate-200'
+            }   cursor-pointer  transition-colors`}
             onClick={() => {
               editCard(cardOptions.id);
             }}
           />
           <Trash
-            className="text-zinc-800 cursor-pointer hover:text-slate-200 transition-colors"
+            className={`${
+              isColorDark
+                ? 'text-white hover:text-zinc-800'
+                : 'text-zinc hover:text-slate-200'
+            }   cursor-pointer  transition-colors`}
             onClick={() => {
               removeCard(cardOptions.id);
             }}
